4cb99f4c1c397a9527624b39addbbeef0df453f1
THUYNT\BAP <leductho911@gmail.com>
2023-02-21 17:24:30 +0700

    commment

--- src/BasicJava/InputAndOutput.java
+++ src/BasicJava/InputAndOutput.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class InputAndOutput {
 	// https://gochocit.com/ngon-ngu-lap-trinh/nhap-va-xuat-co-ban-basic-input-and-output-trong-java
+//	Phương thức	Mô tả
+//	nextByte()	Đọc một số nguyên kiểu byte
+//	nextShort()	Đọc một số nguyên kiểu short
+//	nextInt()	Đọc một số nguyên kiểu int
+//	nextLong()	Đọc một số nguyên kiểu long
+//	nextFloat()	Đọc một số kiểu float
+//	nextDouble()	Đọc một số kiểu double
+//	next()	Đọc một string kết thức trước một ký tự trắng
+//	nextLine()	Đọc một line of text (kết thúc bằng phím Enter)
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
