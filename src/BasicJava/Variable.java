02fde6b0dc25c1501aab229c6d076cfb3e8521d6
THUYNT\BAP <leductho911@gmail.com>
2023-02-27 08:39:37 +0700

    Update

--- src/BasicJava/Variable.java
+++ src/BasicJava/Variable.java
@@ -13,13 +13,13 @@ public class Variable {
         System.out.println("Gia tri cua x la: " + x);
     }
     
-    public int x = 100; // Biến global
+    public  int x = 100; // Biến global
     
      
     public static void main(String[] args) {
     	
         Variable varOne = new Variable(); // tạo 1 biến varOne có kiểu dữ liệu là Variable trùng tên class
-        Variable varTwo = new Variable(); // tạo 1 biến varTwo có kiểu dữ liệu là Variable trùng tên class
+        Variable varTwo = new Variable(); // tạo 1 biến varTwo có kiểu dữ liệu là Variable trùng tên class, 2 object này chiếm vùng nhớ khác nhau
         
                 
         varOne.x++;
